6d899828492a9bf90de57aea5ef014a7dcad62f6
Mann Patel <83982477+mpatel1802@users.noreply.github.com>
2022-06-02 20:13:47 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   clientID: "a490063888e941ed978d004174c343b1",
   clientSecret: "aee0d62569df4545a37f00df1f9d6fa5",
-  userAPIBase: "https://afternoon-cove-08180.herokuapp.com/"
+  userAPIBase: "https://spotify-api-08180.herokuapp.com/"
 };
